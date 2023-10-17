8f0404df3f7eb3956c5a38818fa7d42e37f507e0
Kostya Kartavenka <kkartavenko@gmail.com>
2023-10-17 23:56:03 +0200

    removed comment

--- DataImporter.Tests/InvestingDotComTests.cs
+++ DataImporter.Tests/InvestingDotComTests.cs
@@ -8,7 +8,6 @@ public class InvestingDotComTests
     [Test]
     public void DirectDownloadTest()
     {
-        //https://api.investing.com/api/financialdata/historical/7888?start-date=2023-09-13&end-date=2023-10-13&time-frame=Daily&add-missing-rows=false
         var requestInfo = new OnlineRequestInfo
         {
             Uri = new Uri("https://www.investing.com/equities/exxon-mobil-historical-data"),
